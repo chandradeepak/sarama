8717fc38872dccd78cc0fdeeb4a6b06e7ae0248c
chandra deepak kasiraju <chandra.d.kasiraju@ge.com>
2018-02-18 20:59:14 -0800

    change example server

--- examples/http_server/http_server_test.go
+++ examples/http_server/http_server_test.go
@@ -6,8 +6,8 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"github.com/Shopify/sarama"
-	"github.com/Shopify/sarama/mocks"
+	"github.com/chandradeepak/sarama"
+	"github.com/chandradeepak/sarama/mocks"
 )
 
 // In normal operation, we expect one access log entry,
